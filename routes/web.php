25203a79326fcfc67b027271b0918cfb94a7e64c
Tan <aliyaliyaa00@gmail.com>
2025-10-20 23:28:13 +0700

    feat: add page controller and app routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\PageController.php;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [PageController::class, 'showLogin'])-> name('login');
+Route::get('/dashboard', [PageController::class, 'showDashboard']);
+Route::get('/pengelolaan', [PageController::class, 'showPengelolaan']);
+Route::get('/profile', [PageController::class, 'showProfile']);
+
+Route::post('/login', [PageController::class, 'handleLogin']);
