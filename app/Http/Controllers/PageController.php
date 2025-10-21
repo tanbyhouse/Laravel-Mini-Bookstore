7cb73a2be1f5f37ba7aa55f3ac64e09b4d349ecb
Tan <aliyaliyaa00@gmail.com>
2025-10-21 23:38:15 +0700

    feat: add methods to pageController and create placeholder views

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    // showing login
+    public function showLogin(){
+        return view('login');
+    }
+
+    // processing login 
+    public function handleLogin(Request $request){
+        $username = $request->input('username');
+        return redirect('/dashboard?username=' . $username);
+    }
+
+    // showing dashboard
+    public function showDashboard(){
+        return view('dashboard');
+    }
+
+    // showing dashboard
+    public function showPengelolaan(){
+        return view('pengelolaan');
+    }
+
+    // showing profile
+    public function showProfile(){
+        return view('profile');
+    }
 }
